479d074286daaba6041574821e13756e9ba5d2d1
LeeJungMin1 <jungmin5600@naver.com>
2025-07-15 14:01:02 +0900

    weather 모듈 (service, model) AppError 적용

--- src/api/weather/weather-service.js
+++ src/api/weather/weather-service.js
@@ -1,5 +1,14 @@
 import * as WeatherModel from "./weather-model.js";
+import { AppError } from "../../utils/common-utils/error-handler.js";
 
 export const fetchWeatherInfo = async () => {
-  return await WeatherModel.fetchTodayLatestWeatherInfo();
+  try {
+    return await WeatherModel.fetchTodayLatestWeatherInfo();
+  } catch (error) {
+    throw new AppError(
+      `fetchWeatherInfo 실패: ${error.message}`,
+      500,
+      "FETCH_WEATHER_INFO_FAILED"
+    );
+  }
 };
